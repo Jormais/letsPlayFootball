743a50647395c9f5d6166a220097baced43c4d2d
jormais <jonaybrito15@gmail.com>
2021-06-22 22:20:22 +0100

    se ha completado la vista en detalle basica del jugador

--- src/app/Components/jugador/jugador.component.ts
+++ src/app/Components/jugador/jugador.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Jugadores } from 'src/app/interfaces/jugadores';
+import { JugadorService } from 'src/app/services/jugador.service';
 
 @Component({
   selector: 'app-jugador',
@@ -8,9 +10,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class JugadorComponent implements OnInit {
 
-  constructor(private route : ActivatedRoute) {
-    const id = this.route.snapshot.paramMap.get('id');
-    console.log(id);
+  jugador : Jugadores[] = [];
+
+  constructor(private route : ActivatedRoute, private jugadorService : JugadorService) {
+    const id = this.route.snapshot.paramMap.get('id') as string;
+    jugadorService.sendGetRequest(id).subscribe( jugador => this.jugador = jugador);
   }
 
   ngOnInit(): void {
