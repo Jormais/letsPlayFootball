063be88fb0413b201c97e249850d03ea1b20b074
jormais <jonaybrito15@gmail.com>
2021-06-23 20:53:28 +0100

    se ha añadido la navbar en los demas componentes y se ha pasado el id del jugador al componente que se encarga de editar

--- src/app/Components/add-edit/add-edit.component.ts
+++ src/app/Components/add-edit/add-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-edit',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-edit.component.css']
 })
 export class AddEditComponent implements OnInit {
+  id :string = "";
 
-  constructor() { }
+  constructor(private route : ActivatedRoute) { 
+    this.id = this.route.snapshot.paramMap.get('id') as string;
+  }
 
   ngOnInit(): void {
   }
