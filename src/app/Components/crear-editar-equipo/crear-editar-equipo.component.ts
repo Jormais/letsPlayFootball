28772574c4d6d4aed9c581c285ba1e438db611e5
jormais <jonaybrito15@gmail.com>
2021-06-24 04:06:36 +0100

    se ha creado un formulario para crear equipos con un poco de css

--- src/app/Components/crear-editar-equipo/crear-editar-equipo.component.ts
+++ src/app/Components/crear-editar-equipo/crear-editar-equipo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Equipos } from 'src/app/interfaces/equipos';
 
 @Component({
   selector: 'app-crear-editar-equipo',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearEditarEquipoComponent implements OnInit {
 
+  id :string = "";
+  equipo :Equipos = {
+    'Nombre del equipo': "",
+    id : "",
+    'Logo del Equipo' : "",
+    Liga : ""
+}
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(){
+
   }
 
 }
