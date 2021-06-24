1a256b1ec6403b4dad02d3502f8e7b2ec33db827
jormais <jonaybrito15@gmail.com>
2021-06-24 01:22:23 +0100

    se ha añadido un archivo de configuracion para poder hacer post y el metodo para guardar el post

--- src/app/Components/add-edit/add-edit.component.ts
+++ src/app/Components/add-edit/add-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Jugadores } from 'src/app/interfaces/jugadores';
+import { JugadoresService } from 'src/app/services/jugadores/jugadores.service';
 
 @Component({
   selector: 'app-add-edit',
@@ -8,12 +10,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AddEditComponent implements OnInit {
   id :string = "";
+  jugador : Jugadores = {
+    'Nombre del Jugador' : "jonay",
+    id : "123456789",
+    Avatar : "https://steamavatar.io/img/14773518797UrEY.png",
+    teamId:"8154f4cb-246b-4bf9-bc64-51f8661b6781"
+  }
 
-  constructor(private route : ActivatedRoute) { 
+  constructor(private route : ActivatedRoute, private juadoresService : JugadoresService) { 
     this.id = this.route.snapshot.paramMap.get('id') as string;
   }
 
+  
+
   ngOnInit(): void {
+  }
+
+  guardar() {
+    this.juadoresService.sendPostRequest(this.jugador).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
