3044bceb9362690dd04fd72fe3cda37e3104ef3f
jormais <jonaybrito15@gmail.com>
2021-07-20 14:33:07 +0100

    se ha arreglado un bug que se genero al crear los entornos y se ha cambiado el nombre del archivo db.json que antes se llamaba bd.json erroneamente.

--- src/app/services/jugador/jugador.service.ts
+++ src/app/services/jugador/jugador.service.ts
@@ -15,7 +15,7 @@ export class JugadorService {
   constructor(public httpClient: HttpClient) { }
 
   sendGetRequestByID(id : string) : Observable<Jugadores[]> {
-    return this.httpClient.get<Jugadores[]>(this.apiUrl  + `?id=${id}`);
+    return this.httpClient.get<Jugadores[]>(this.apiUrl  + `players?id=${id}`);
   }
   
   sendGetRequest() : Observable<Jugadores[]> {
